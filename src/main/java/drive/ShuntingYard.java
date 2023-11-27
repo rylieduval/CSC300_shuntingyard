1a7b879c01d2a02542b914f54983faa5ca3b4c3b
rylieduval <129247479+rylieduval@users.noreply.github.com>
2023-11-26 20:52:41 -0600

    Update ShuntingYard.java

--- src/main/java/drive/ShuntingYard.java
+++ src/main/java/drive/ShuntingYard.java
@@ -13,30 +13,29 @@ public class ShuntingYard {
     public void parse(String input){
         String numbers = "0123456789";
 		String temp = "";
-		
-		for (int i = 0; i < input.length(); i++)
+	
+	for (int i = 0; i < input.length(); i++)
+	{
+	if (numbers.indexOf(input.charAt(i)) >= 0)
+	{
+		temp+= input.charAt(i);
+	}
+	else 
+	{
+		if (temp.length() > 0)
 		{
-			if (numbers.indexOf(input.charAt(i)) >= 0)
-			{
-				temp+= input.charAt(i);
-			}
-			else 
-			{
-				if (temp.length() > 0)
-				{
-					this.Tokens.append(temp);
-				}
-				
-				temp = "";
-				this.Tokens.append(Character.toString(input.charAt(i)));
-			}
+			this.Tokens.append(temp);
 		}
 		
-		if (temp != null && temp.length() > 0)
-		{
-			this.Tokens.append(temp)
-		}
-    }
+		temp = "";
+		this.Tokens.append(Character.toString(input.charAt(i)));
+	}
+	}
+	if (temp != null && temp.length() > 0)
+	{
+		this.Tokens.append(temp)
+	}
+}
 
     public void process()
     {
@@ -82,31 +81,31 @@ public class ShuntingYard {
         int x;
         int y;
         while(!ReversePolish.isEmpty()) {
-        	temp = ReversePolish.dequeue().Data;
-        	if(temp.indexOf(s) >= 0) {
-        		x = Integer.parseInt(tempstack.pop().Data);
-        		y = Integer.parseInt(tempstack.pop().Data);
-                switch (temp.charAt(0)) {
-                    case '*':
-                        x = y * x;
-                        break;
-                    case '/':
-                        x = y / x;
-                        break;
-                    case '+':
-                        x = y + x;
-                        break;
-                    case '-':
-                        x = y - x;
-                        break;
-                    default:
-                        break;
-                }
-        		tempstack.push(Integer.toString(x));
-        	} else {
-        		tempstack.push(temp);
-        	}
-        }
+	temp = ReversePolish.dequeue().Data;
+	if(temp.indexOf(s) >= 0) {
+		x = Integer.parseInt(tempstack.pop().Data);
+		y = Integer.parseInt(tempstack.pop().Data);
+	switch (temp.charAt(0)) {
+	    case '*':
+		x = y * x;
+		break;
+	    case '/':
+		x = y / x;
+		break;
+	    case '+':
+		x = y + x;
+		break;
+	    case '-':
+		x = y - x;
+		break;
+	    default:
+		break;
+	}
+		tempstack.push(Integer.toString(x));
+	} else {
+		tempstack.push(temp);
+	}
+}
         return Integer.parseInt(tempstack.pop().Data);
     }  
 }
