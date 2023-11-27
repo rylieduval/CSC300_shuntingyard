2b4f429ff5fdc5fa7238332029f34c4a36cd8ed6
rylieduval <129247479+rylieduval@users.noreply.github.com>
2023-11-26 20:51:39 -0600

    Update ShuntingYard.java

--- src/main/java/drive/ShuntingYard.java
+++ src/main/java/drive/ShuntingYard.java
@@ -10,47 +10,103 @@ public class ShuntingYard {
         this.ReversePolish = new OutputQueue();
     }
 
-    //parse a math expression into a linked list
-    //input: the math expression as a string
-    //parsed result will stored in Tokens linked list
     public void parse(String input){
-
+        String numbers = "0123456789";
+		String temp = "";
+		
+		for (int i = 0; i < input.length(); i++)
+		{
+			if (numbers.indexOf(input.charAt(i)) >= 0)
+			{
+				temp+= input.charAt(i);
+			}
+			else 
+			{
+				if (temp.length() > 0)
+				{
+					this.Tokens.append(temp);
+				}
+				
+				temp = "";
+				this.Tokens.append(Character.toString(input.charAt(i)));
+			}
+		}
+		
+		if (temp != null && temp.length() > 0)
+		{
+			this.Tokens.append(temp)
+		}
     }
 
-    /*
-     * 1.  While there are tokens to be read:
-     * 2.        Read a token
-     * 3.        If it's a number add it to queue
-     * 4.        If it's an operator
-     * 5.               While there's an operator on the top of the stack with greater precedence:
-     * 6.                       Pop operators from the stack onto the output queue
-     * 7.               Push the current operator onto the stack
-     * 8.        If it's a left bracket push it onto the stack
-     * 9.        If it's a right bracket 
-     * 10.            While there's not a left bracket at the top of the stack:
-     * 11.                     Pop operators from the stack onto the output queue.
-     * 12.             Pop the left bracket from the stack and discard it
-     * 13. While there are operators on the stack, pop them to the queue
-     */
-    //take the tokens from Tokens queue, and stored the reversed polish expression in ReversePolish queue
-    public void process(){
-        //to do
+    public void process()
+    {
+    OpStack operatorStack = new OpStack();
+    Node current = this.Tokens.Head;
+
+    while (current != null) {
+        char currentChar = current.Data.charAt(0);
+
+        if (currentChar == '(') {
+            operatorStack.push(current.Data);
+        } else if (currentChar == ')') {
+            while (!operatorStack.isEmpty() && operatorStack.peek().Data.charAt(0) != '(') {
+                queue.enqueue(operatorStack.pop().Data);
+            }
+
+            if (!operatorStack.isEmpty() && operatorStack.peek().Data.charAt(0) == '(') {
+                operatorStack.pop();
+            }
+        } else if (isOperator(currentChar)) {
+            if (currentChar == '+' || currentChar == '-') {
+                while (!operatorStack.isEmpty() && (operatorStack.peek().Data.charAt(0) == '*' || operatorStack.peek().Data.charAt(0) == '/')) {
+                    this.queue.enqueue(operatorStack.pop().Data);
+                }
+            }
+            operatorStack.push(current.Data);
+        } else {
+            this.queue.enqueue(current.Data);
+        }
+
+        current = current.NextNode;
     }
 
-    /*
-     * 1. Tokens on queue dequeue() one by one
-     * 2. Each time a number or operand dequeue(), push it to the stack.
-     * 3. Each time an operator comes up, pop() operands from the stack, perform the operations,
-              and push the result back to the stack.
-     * 4. Finished when there are no tokens to read. 
-     * 5. The final number on the stack is the result.
-     */
-    //process use the reverse polish format of expression to process the math result
-    //output: the math result of the expression
-    public int run(){
-        //to do
-        throw new Error("waiting for implement");
+    while (!symbs.isEmpty()) {
+        queue.enqueue(symbs.pop().Data);
     }
-
+    }
     
+    public int run(){
+        OpStack tempstack = new OpStack();
+        String temp;
+        String s = "*+/-";
+        int x;
+        int y;
+        while(!ReversePolish.isEmpty()) {
+        	temp = ReversePolish.dequeue().Data;
+        	if(temp.indexOf(s) >= 0) {
+        		x = Integer.parseInt(tempstack.pop().Data);
+        		y = Integer.parseInt(tempstack.pop().Data);
+                switch (temp.charAt(0)) {
+                    case '*':
+                        x = y * x;
+                        break;
+                    case '/':
+                        x = y / x;
+                        break;
+                    case '+':
+                        x = y + x;
+                        break;
+                    case '-':
+                        x = y - x;
+                        break;
+                    default:
+                        break;
+                }
+        		tempstack.push(Integer.toString(x));
+        	} else {
+        		tempstack.push(temp);
+        	}
+        }
+        return Integer.parseInt(tempstack.pop().Data);
+    }  
 }
