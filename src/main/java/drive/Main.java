abff1b8b2105ead927d9d4790eb2bb6028dc3c47
rylieduval <129247479+rylieduval@users.noreply.github.com>
2023-11-26 20:51:16 -0600

    Update Main.java

--- src/main/java/drive/Main.java
+++ src/main/java/drive/Main.java
@@ -4,9 +4,7 @@ public class Main {
     public static void main(String[] args) {
         ShuntingYard a = new ShuntingYard();
         a.parse("(4+3)*5+(6-1)");
-        System.out.println(a.Tokens);
         a.process();
-        System.out.println("Reversed Polish: " + a.ReversePolish)
-        System.out.println("Expressioin result is: " + a.run());
+        System.out.println("Result is: " + a.run());
     }
 }
